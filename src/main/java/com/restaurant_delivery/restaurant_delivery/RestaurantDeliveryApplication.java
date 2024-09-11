939cdb79cc7cb7e2014b41651bc6671c28a7bdcb
LCzopko <lubomirczopko@gmail.com>
2024-09-11 13:09:05 +0200

    first commit

---
+++ src/main/java/com/restaurant_delivery/restaurant_delivery/RestaurantDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant_delivery.restaurant_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantDeliveryApplication.class, args);
+	}
+
+}
